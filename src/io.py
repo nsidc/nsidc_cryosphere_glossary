e13e43c6bc3657af362b1870c01696eecea93e7a
Andy Barrett <apbarret@nsidc.org>
2024-06-12 18:17:53 -0600

    initial commit

---
+++ src/io.py
@@ -0,0 +1,10 @@
+"""Input and output methods for glossary"""
+from Typing import List
+from pathlib import Path
+
+def load_glossary_csv(filepath: Path) -> List[]:
+    """Loads a csv file containing a glossary and returns a list of
+    glossary items"""
+    
+
+    
