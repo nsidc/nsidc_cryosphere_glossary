3164540756e40304caf6fd5e4979f4ed9f5aa621
Andy Barrett <apbarret@nsidc.org>
2024-06-12 18:33:40 -0600

    fix typo

--- src/io.py
+++ src/io.py
@@ -1,10 +1,16 @@
 """Input and output methods for glossary"""
-from Typing import List
+from typing import List
 from pathlib import Path
 
-def load_glossary_csv(filepath: Path) -> List[]:
+def load_glossary_csv(filepath: Path, header=0, skiprows=0) -> List:
     """Loads a csv file containing a glossary and returns a list of
     glossary items"""
-    
-
-    
+    entries = []
+    with open(filepath) as f:
+        lines = f.readlines()
+    for i, line in enumerate(lines):
+        if i <= skiprows:
+            continue
+        if i == (header + skiprows):
+            columns = line.split(",")
+        
