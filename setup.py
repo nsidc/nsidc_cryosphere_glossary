e8b0c2ea8b114dcb3da5765f98cabfdbe62cba29
Andy Barrett <apbarret@nsidc.org>
2024-06-12 18:57:14 -0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nsidc-cryosphere-glossary',
+    version='1.0.0',
+#    url='https://github.com/mypackage.git',
+    author='Andy Barrett',
+    author_email='andrew.barrett@colorado.edu',
+    description='Tools to manage NSIDC Cryosphere Glossary',
+    packages=find_packages(),    
+)
