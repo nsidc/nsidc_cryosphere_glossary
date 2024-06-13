26f6fae5f3feefd57fa45507427c025c1a1c1940
Andy Barrett <apbarret@nsidc.org>
2024-06-12 19:09:02 -0600

    fix typo

--- tests/test_io.py
+++ tests/test_io.py
@@ -5,6 +5,6 @@ from src.io import parse_entry_csv
 line = '''ablation,"(1) combined processes (such as sublimation, fusion or melting, evaporation) which remove snow or ice from the surface of a glacier or from a snow-field; also used to express the quantity lost by these processes (2) reduction of the water equivalent of a snow cover by melting, evaporation, wind and avalanches."
 '''
 
-def test_parse_entry_csv(line):
+def test_parse_entry_csv():
     entry = parse_entry_csv(line)
     print(entry)
