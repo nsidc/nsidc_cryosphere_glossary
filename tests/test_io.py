7a36623ff72b06ade6b1c33377aa323bcfa9de5b
Andy Barrett <apbarret@nsidc.org>
2024-06-14 18:13:37 -0600

    add expected result from parse entry

--- tests/test_io.py
+++ tests/test_io.py
@@ -4,7 +4,19 @@ from src.io import parse_entry_csv
 
 line = '''ablation,"(1) combined processes (such as sublimation, fusion or melting, evaporation) which remove snow or ice from the surface of a glacier or from a snow-field; also used to express the quantity lost by these processes (2) reduction of the water equivalent of a snow cover by melting, evaporation, wind and avalanches."
 '''
+expected_entry_csv = {
+    "term": "ablation",
+    "definition": {
+        1: "Combined processes (such as sublimation, fusion or melting, evaporation) which remove snow or ice from the surface of a glacier or from a snow-field; also used to express the quantity lost by these processes.",
+        2: "Reduction of the water equivalent of a snow cover by melting, evaporation, wind and avalanches."
+        }
+    }
+
 
 def test_parse_entry_csv():
-    entry = parse_entry_csv(line)
-    print(entry)
+    result = parse_entry_csv(line)
+    assert result == expected_entry_csv
+
+
+#def test_load_csv():
+#    
